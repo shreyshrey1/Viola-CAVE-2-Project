479253993be8a8af6c8b27c21c0ad48334423bc2
shreyshrey1 <talk2shreyas@gmail.com>
2017-07-20 21:22:07 +0530

    VR Program Files

---
+++ Virtual Violin Instructor/Assets/Scripts/Rotator2.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator2 : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		// Rotate the object around its local Y axis at 1 degree per second
+		transform.Rotate(30*Vector3.up * Time.deltaTime);
+	}
+}
---
+++ Virtual Violin Instructor/Assets/countdown.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	public float time = 3.0f; // Arbitrary number
+	TextMesh tm;
+
+	void Start ()
+    {
+		tm = GetComponent<TextMesh>();
+	}
+	void Update () {
+		time -= Time.deltaTime;
+		if(time > 0)
+        {
+			//If time is greater than 0, display time left
+			tm.text = Mathf.RoundToInt(time).ToString();
+		}
+        else
+		{
+			//Otherwise display this
+			tm.text = "GO";
+		}
+
+	}
+}
